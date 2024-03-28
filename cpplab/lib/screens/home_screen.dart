f1059de4e410fb99388008cc64b36e6c7c8e8b9a
vladost03 <99389693+vladost03@users.noreply.github.com>
2024-03-28 21:30:39 +0200

    Update

--- cpplab/lib/screens/home_screen.dart
+++ cpplab/lib/screens/home_screen.dart
@@ -5,23 +5,53 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Головна сторінка'),
+        title: Text('Авторизація', style: TextStyle(color: Colors.grey)),
+        backgroundColor: Colors.green[200], // Сіро-зелений колір для AppBar
       ),
-      body: Center(
+      body: Container(
+        padding: EdgeInsets.all(20.0),
+        color: Colors.grey[200], // Сірий фон для body
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'Ласкаво просимо до нашого магазину продуктів!',
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 20.0),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Логін',
+                fillColor: Colors.white,
+                filled: true,
+              ),
+            ),
+            SizedBox(height: 20.0),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Пароль',
+                fillColor: Colors.white,
+                filled: true,
+              ),
+              obscureText: true,
             ),
             SizedBox(height: 20.0),
             ElevatedButton(
               onPressed: () {
                 // Дії, які виконуються при натисканні кнопки
               },
-              child: Text('Переглянути продукти'),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.green[200], // Світло-зелений колір для кнопки
+              ),
+              child: Text(
+                'Вхід',
+                style: TextStyle(color: Colors.grey), // Сірий колір для тексту кнопки
+              ),
+            ),
+            SizedBox(height: 10.0),
+            GestureDetector(
+              onTap: () {
+                // Дії, які виконуються при натисканні посилання на реєстрацію
+              },
+              child: Text(
+                'Ще не зареєстровані? Зареєструватися',
+                style: TextStyle(color: Colors.blue),
+              ),
             ),
           ],
         ),
